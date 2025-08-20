f61de012236a1abc1113febcafb47b7b4f1d0b1d
Bipin Thapa <yukichin638@gmail.com>
2025-08-20 12:20:46 +0545

    Nav Added for mobile

--- src/app/nav.js
+++ src/app/nav.js
@@ -7,9 +7,34 @@ import { IoPerson } from "react-icons/io5";
 import { RxHamburgerMenu } from "react-icons/rx";
 import { ImCross } from "react-icons/im";
 import "./global.css";
+import { useState, useEffect } from "react";
 
 export default function Nav() {
   const router = useRouter();
+
+  const [burger, setBurger] = useState(null);
+  const [rightNav, setRightNav] = useState(null);
+
+  useEffect(() => {
+    setBurger(document.getElementById('burger'));
+    setRightNav(document.getElementById('right-nav'));
+  }, [])
+
+  const showNav = () => {
+    if (burger && rightNav) {
+      burger.style.display = "none";
+      rightNav.style.display = "flex";
+    }
+  }
+
+  const hideNav = () => {
+    if (burger && rightNav) {
+      burger.style.display = "block";
+      rightNav.style.display = "none";
+    }
+  }
+
+
   return (
     <div className="nav">
       <div className="logo" onClick={() => router.push("/")}>
@@ -30,18 +55,26 @@ export default function Nav() {
         </div>
       </div>
       <div className="icon-container2">
-        <div className="icon-2">
-          <RxHamburgerMenu className="burger" />
-          <ImCross className="cross" />
-        </div>
-        <div className="icon icon1" onClick={() => { router.push('/') }}>
-          <div className="icon-name2">Home</div>
-        </div>
-        <div className="icon icon2" onClick={() => { router.push('/history') }}>
-          <div className="icon-name2">History</div>
-        </div>
-        <div className="icon" onClick={() => { router.push('/profile') }}>
-          <div className="icon-name2">Profile</div>
+        <RxHamburgerMenu id="burger" onClick={() => { showNav() }} />
+        <div id="right-nav">
+          <div className="right-nav-cont">
+            <ImCross className="cross" onClick={() => { hideNav() }} />
+            <div className="font">
+              Subcryption
+            </div>
+          </div>
+          <div className="icon r" onClick={() => { router.push('/') }}>
+            <AiFillHome className="home-icon" />
+            <div className="icon-name2">Home</div>
+          </div>
+          <div className="icon r" onClick={() => { router.push('/history') }}>
+            <MdOutlineHistory className="history-icon" />
+            <div className="icon-name2">History</div>
+          </div>
+          <div className="icon r" onClick={() => { router.push('/profile') }}>
+            <IoPerson className="profile-icon" />
+            <div className="icon-name2">Profile</div>
+          </div>
         </div>
       </div>
     </div>
