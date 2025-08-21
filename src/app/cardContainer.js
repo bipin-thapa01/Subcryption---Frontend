4d00783809c1f0750a4c35d27ad18c838c627eea
Bipin Thapa <yukichin638@gmail.com>
2025-08-21 22:16:52 +0545

    push moved from localhost to global

--- src/app/cardContainer.js
+++ src/app/cardContainer.js
@@ -1,48 +1,56 @@
-import { useState, useEffect, useImperativeHandle, forwardRef } from "react";
+"use client";
+
+import { useState, useEffect, useRef, useImperativeHandle } from "react";
 import Card from "./card";
+import Image from "next/image";
 import "./card.css";
 
-const CardContainer = forwardRef((props, ref) => {
+export default function CardContainer({ ref }) {
   const [cardDetails, setCardDetails] = useState(null);
   const [cards, setCards] = useState(null);
 
   const filterCard = (type) => {
-    if (!cardDetails) return;
-    if (type === "all") {
-      setCards(cardDetails.map((item, index) => (
-        <Card key={index} param={item} />
-      )));
-    } else {
-      setCards(
-        cardDetails
-          .filter((item) => item.type.toLowerCase() === type)
-          .map((item, index) => <Card key={index} param={item} />)
-      );
+    if (cardDetails != null) {
+      if (type === 'all') {
+        setCards(cardDetails.map((item, index) => {
+          return <Card key={index} param={item} />
+        }));
+      }
+      else {
+        setCards(cardDetails.filter((item) => {
+          return item.type.toLowerCase() === type;
+        }).map((item, index) => {
+          return <Card key={index} param={item} />
+        }));
+      }
     }
-  };
+  }
 
   useImperativeHandle(ref, () => ({
-    filterCard,
+    filterCard: filterCard,
   }));
 
   useEffect(() => {
     const fetchData = async () => {
-      const res = await fetch("https://subcryption-backend.onrender.com/card");
+      const res = await fetch('https://subcryption-backend.onrender.com/card');
       const data = await res.json();
       setCardDetails(data);
-    };
+    }
+
     fetchData();
   }, []);
 
   useEffect(() => {
-    if (cardDetails) {
-      setCards(cardDetails.map((item, index) => (
-        <Card key={index} param={item} />
-      )));
+    if (cardDetails != null) {
+      setCards(cardDetails.map((item, index) => {
+        return <Card key={index} param={item} />
+      }));
     }
   }, [cardDetails]);
 
-  return <div className="card-container">{cards}</div>;
-});
-
-export default CardContainer;
+  return (
+    <div className="card-container">
+      {cards}
+    </div>
+  );
+}
